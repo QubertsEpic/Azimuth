4045dba5b4e374f50b87716185329ca633ed7ba3
Matthew Findlay <matthewfindlay06@gmail.com>
2022-03-25 19:10:33 +0000

    Added cross-thread referencing locking.

--- FlightRewinder.SimConnectWrapper/Core/Connection.cs
+++ FlightRewinder.SimConnectWrapper/Core/Connection.cs
@@ -23,7 +23,7 @@ namespace SimConnectWrapper.Core
         public event EventHandler? Quit;
         public event EventHandler? FrameEvent;
         public event EventHandler<LocationChangedEventArgs>? LocationChanged;
-
+        public object lockObject = new();
         const int WM_USER_SIMCONNECT = 0x0402;
 
 
@@ -34,37 +34,46 @@ namespace SimConnectWrapper.Core
 
         public void Initialise(IntPtr handle)
         {
-            instance = new SimConnect("Flight Rewinder", handle, WM_USER_SIMCONNECT, null, 0);
+            lock (lockObject)
+            {
+                instance = new SimConnect("Flight Rewinder", handle, WM_USER_SIMCONNECT, null, 0);
 
-            instance.OnRecvOpen += new SimConnect.RecvOpenEventHandler(Connection_OnOpen);
-            instance.OnRecvQuit += new SimConnect.RecvQuitEventHandler(Connection_OnQuit);
+                instance.OnRecvOpen += new SimConnect.RecvOpenEventHandler(Connection_OnOpen);
+                instance.OnRecvQuit += new SimConnect.RecvQuitEventHandler(Connection_OnQuit);
 
-            instance.OnRecvException += Instance_OnRecvException;
-            instance.OnRecvSimobjectData += Instance_OnRecvSimobjectData;
-            instance.OnRecvEventFrame += Instance_OnRecvEventFrame;
-            instance.AddToDataDefinition(Definitions.InitialPosition, "Initial Position", null, SIMCONNECT_DATATYPE.INITPOSITION, 0f, SimConnect.SIMCONNECT_UNUSED);
+                instance.OnRecvException += Instance_OnRecvException;
+                instance.OnRecvSimobjectData += Instance_OnRecvSimobjectData;
+                instance.OnRecvEventFrame += Instance_OnRecvEventFrame;
+                instance.AddToDataDefinition(Definitions.InitialPosition, "Initial Position", null, SIMCONNECT_DATATYPE.INITPOSITION, 0f, SimConnect.SIMCONNECT_UNUSED);
 
-            instance.SubscribeToSystemEvent(Events.FRAME, "Frame");
-            PrepareData();
+                instance.SubscribeToSystemEvent(Events.FRAME, "Frame");
+                PrepareData();
 
-            StartDataTransfer();
+                StartDataTransfer();
 
-            Initialised?.Invoke(this, new EventArgs());
+                Initialised?.Invoke(this, new EventArgs());
+            }
         }
 
         private void Instance_OnRecvEventFrame(SimConnect sender, SIMCONNECT_RECV_EVENT_FRAME data)
         {
-            FrameEvent?.Invoke(this, new());
+            lock (lockObject)
+            {
+                FrameEvent?.Invoke(this, new());
+            }
         }
 
         private void Instance_OnRecvSimobjectData(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA data)
         {
-            if (data.dwRequestID == ((uint)Requests.PlaneLocation))
+            lock (lockObject)
             {
-                var position = data.dwData[0] as PositionStruct?;
-                if (position.HasValue)
+                if (data.dwRequestID == ((uint)Requests.PlaneLocation))
                 {
-                    AircraftPositionUpdate(position.Value);
+                    var position = data.dwData[0] as PositionStruct?;
+                    if (position.HasValue)
+                    {
+                        AircraftPositionUpdate(position.Value);
+                    }
                 }
             }
         }
@@ -100,7 +109,10 @@ namespace SimConnectWrapper.Core
 
         public void Init(uint planeID, PositionStruct position)
         {
-            instance?.SetDataOnSimObject(Definitions.InitialPosition, planeID, SIMCONNECT_DATA_SET_FLAG.DEFAULT, PositionStruct.PosStructToInitPos(position));
+            lock (lockObject)
+            {
+                instance?.SetDataOnSimObject(Definitions.InitialPosition, planeID, SIMCONNECT_DATA_SET_FLAG.DEFAULT, PositionStruct.PosStructToInitPos(position));
+            }
         }
 
         private void Connection_OnQuit(SimConnect sender, SIMCONNECT_RECV data)
@@ -111,15 +123,18 @@ namespace SimConnectWrapper.Core
 
         public void Close()
         {
-            if (instance != null)
+            lock (lockObject)
             {
-                Console.WriteLine("Simconnect has exited.");
-                instance.Dispose();
-                instance = null;
-            }
-            else
-            {
-                Console.WriteLine("An error has occurred.");
+                if (instance != null)
+                {
+                    Console.WriteLine("Simconnect has exited.");
+                    instance.Dispose();
+                    instance = null;
+                }
+                else
+                {
+                    Console.WriteLine("An error has occurred.");
+                }
             }
         }
 
@@ -135,91 +150,114 @@ namespace SimConnectWrapper.Core
         /// <returns></returns>
         public uint CreatePlane(string title, PositionStruct position)
         {
-            var request = Requests.CreatePlane + RequestCount;
-            RequestCount = (RequestCount++) % 10000;
-            try
+            lock (lockObject)
             {
-                SIMCONNECT_DATA_INITPOSITION initPosition = new SIMCONNECT_DATA_INITPOSITION()
+                var request = Requests.CreatePlane + RequestCount;
+                RequestCount = (RequestCount++) % 10000;
+                try
                 {
-                    Altitude = position.Altitude,
-                    Longitude = position.Longitude,
-                    Latitude = position.Latitude,
-                    Bank = position.Bank,
-                    Heading = position.Heading,
-                    Pitch = position.Pitch,
-                };
-                instance?.AICreateNonATCAircraft(title, "Rewind", initPosition, request);
-                return RequestCount;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-                throw;
+                    SIMCONNECT_DATA_INITPOSITION initPosition = new SIMCONNECT_DATA_INITPOSITION()
+                    {
+                        Altitude = position.Altitude,
+                        Longitude = position.Longitude,
+                        Latitude = position.Latitude,
+                        Bank = position.Bank,
+                        Heading = position.Heading,
+                        Pitch = position.Pitch,
+                        Airspeed = 0,
+                        OnGround = position.OnGround
+                    };
+                    instance?.AICreateNonATCAircraft(title, "Rewind", initPosition, request);
+                    return RequestCount;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                    throw;
+                }
             }
         }
 
         public void SetData(uint aircraftID, Enum definition, object dataToSet)
         {
-            try
+            lock (lockObject)
             {
-                instance?.SetDataOnSimObject(definition, aircraftID, SIMCONNECT_DATA_SET_FLAG.DEFAULT, dataToSet);
-            } 
-            catch (Exception)
-            {
-                Console.WriteLine("Error while setting data.");
-                return;
+                try
+                {
+                    instance?.SetDataOnSimObject(definition, aircraftID, SIMCONNECT_DATA_SET_FLAG.DEFAULT, dataToSet);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error while setting data.");
+                    return;
+                }
             }
         }
 
         public void RemovePlane(uint aircraftID)
         {
-            try
+            lock (lockObject)
             {
-                instance?.AIRemoveObject(aircraftID, Requests.RemovePlane);
-            }
-            catch (Exception) 
-            {
-                Console.WriteLine("Error while removing plane, possibly missing aircraftID");
-                return;
+                try
+                {
+                    instance?.AIRemoveObject(aircraftID, Requests.RemovePlane);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error while removing plane, possibly missing aircraftID");
+                    return;
+                }
             }
         }
 
         public void AddToDataDefinition<T>(Enum definition, params (string name, string unit, SIMCONNECT_DATATYPE dataType)[] objects)
         {
-            if (objects == null)
-                return;
-            foreach (var obj in objects)
+            lock (lockObject)
             {
-                instance?.AddToDataDefinition(definition, obj.name, obj.unit, obj.dataType, 0, SimConnect.SIMCONNECT_UNUSED);
+                if (objects == null)
+                    return;
+                foreach (var obj in objects)
+                {
+                    instance?.AddToDataDefinition(definition, obj.name, obj.unit, obj.dataType, 0, SimConnect.SIMCONNECT_UNUSED);
+                }
+                instance?.RegisterDataDefineStruct<T>(definition);
             }
-            instance?.RegisterDataDefineStruct<T>(definition);
         }
 
         public void AddToDataDefinition<T>(Enum definition, List<DefinitionAttribute> arrays)
         {
-            if (arrays == null)
-                throw new ArgumentNullException(nameof(arrays), "Cannot use null arrays.");
-            foreach (var obj in arrays)
+            lock (lockObject)
             {
-                instance?.AddToDataDefinition(definition, obj.VariableName, obj.Unit, obj.DataType, 0, SimConnect.SIMCONNECT_UNUSED);
+                if (arrays == null)
+                    throw new ArgumentNullException(nameof(arrays), "Cannot use null arrays.");
+                foreach (var obj in arrays)
+                {
+                    instance?.AddToDataDefinition(definition, obj.VariableName, obj.Unit, obj.DataType, 0, SimConnect.SIMCONNECT_UNUSED);
+                }
+                instance?.RegisterDataDefineStruct<T>(definition);
             }
-            instance?.RegisterDataDefineStruct<T>(definition);
         }
 
         public void StartDataTransfer()
         {
-            if (instance == null)
-                throw new NullReferenceException("Cannott use null sim connect instance.");
-            instance.RequestDataOnSimObject(Requests.PlaneLocation, Definitions.LocationStruct, 0, SIMCONNECT_PERIOD.SIM_FRAME, SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT, 0, 0, 0);
+            lock (lockObject)
+            {
+                if (instance == null)
+                    throw new NullReferenceException("Cannott use null sim connect instance.");
+                instance.RequestDataOnSimObject(Requests.PlaneLocation, Definitions.LocationStruct, 0, SIMCONNECT_PERIOD.SIM_FRAME, SIMCONNECT_DATA_REQUEST_FLAG.DEFAULT, 0, 0, 0);
+            }
         }
 
         public void PrepareData()
         {
-            if (instance == null)
-                return;
-            List<DefinitionAttribute> attributes = PositionStruct.GetAllAttributes();
-            AddToDataDefinition<PositionStruct>(Definitions.LocationStruct, attributes);
-            AddToDataDefinition<PositionStruct>(Definitions.SetLocation, attributes);
+            lock (lockObject)
+            {
+                if (instance == null)
+                    return;
+                List<DefinitionAttribute> attributes = PositionStruct.GetAllAttributes();
+                AddToDataDefinition<PositionStruct>(Definitions.LocationStruct, attributes);
+                AddToDataDefinition<PositionStruct>(Definitions.SetLocation, attributes);
+            }
         }
     }
 }
